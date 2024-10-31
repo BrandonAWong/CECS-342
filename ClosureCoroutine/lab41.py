eee976206037b5c6d735ccf6919e5e81e57a9bb3
BrandonAWong <105482178+BrandonAWong@users.noreply.github.com>
2024-10-30 19:01:41 -0700

    mc

--- ClosureCoroutine/lab41.py
+++ ClosureCoroutine/lab41.py
@@ -1,5 +1,10 @@
+from datetime import datetime
+
 def check_authorized(func):
     def wrapper():
+        # 7AM - 10PM
+        if not (7 <= datetime.now().hour <= 22):
+            return "It is currently not day."
         if not is_authorized():
             message = "you are not authorized"
         else:
@@ -22,3 +27,4 @@ def Do_B():
 def Do_C():
     return "Do C"
 
+print(Do_A(), Do_B(), Do_C(), sep="\n")
